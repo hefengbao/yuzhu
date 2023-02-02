e6be23c05ce7216d4f3847ea9dfeba6cfbdfd3a3
hefengbao <754582231@qq.com>
2023-02-02 17:47:31 +0800

    select 添加到本地替换第三方CDN

--- resources/views/admin/common/tags_select.blade.php
+++ resources/views/admin/common/tags_select.blade.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" integrity="sha512-nMNlpuaDPrqlEls3IX/Q56H36qvBASwb3ipuo3MxeWbsQB1881ox0cRv7UPTgBlriqoynt35KjEwgGUeUXIPnw==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js" integrity="sha512-2ImtlRlf2VVmiGZsjm9bEyhjGW4dU7B6TNwh/hx/iSByxNENtj3WVE6o/9Lj4TJeVXPi4bnOIMXFIJJAeufa0A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<link rel="stylesheet" href="{{ asset('libs/select2/4.0.13/css/select2.min.css') }}"/>
+<script src="{{ asset('libs/select2/4.0.13/js/select2.min.js') }}"></script>
 <script type="text/javascript">
     $(document).ready(function () {
         $("#tags").select2({
