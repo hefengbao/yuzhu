dd66d1f0a1a034b8c6fb53fdc0eecdfd6bcbb310
hefengbao <754582231@qq.com>
2023-02-08 12:25:17 +0800

    update: 更新评论通知邮件模板

--- resources/views/emails/post_commented.blade.php
+++ resources/views/emails/post_commented.blade.php
@@ -1,1 +1,19 @@
+<p>
+    @php
+    $post = $comment->post;
+    @endphp
+    <a href="@if($post->type == \App\Constant\PostType::Article->value)
+    {{ route('articles.show',$post->slug_id) }}#comment-{{$comment->id}}
+    @elseif($post->type == \App\Constant\PostType::Tweet->value)
+    {{ route('tweets.show',$post->slug_id) }}#comment-{{$comment->id}}
+    @else
+    {{ route('pages.show',$post->slug_id) }}#comment-{{$comment->id}}
+    @endif" target="_blank">
+        @if($comment->user)
+            {{ '@'.$comment->user->name }}
+        @else
+            {{ '@'.$comment->guest_name }}
+        @endif
+    </a> :
+</p>
 {!! \Illuminate\Support\Facades\App::make(\App\One\MarkdownToHtml::class)->convert($comment->body) !!}
