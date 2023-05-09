8a35f79a841910e7926f53aad1721091577bee80
hefengbao <754582231@qq.com>
2023-05-09 17:04:52 +0800

    fix: 标签显示重叠

--- resources/views/themes/default/search/index.blade.php
+++ resources/views/themes/default/search/index.blade.php
@@ -5,11 +5,11 @@
     <div class="mb-4">
         <h3>分类</h3>
         @foreach($categories as $category)
-            <a href="{{ route('search.categories', $category->slug) }}" target="_blank"><span class="badge rounded-pill text-bg-secondary p-2">{{ $category->name }}</span></a>
+            <a href="{{ route('search.categories', $category->slug) }}" target="_blank"><span class="badge rounded-pill text-bg-secondary p-2 m-2">{{ $category->name }}</span></a>
         @endforeach
         <h3 class="py-3">标签</h3>
         @foreach($tags as $tag)
-            <a href="{{ route('search.tags', $tag->slug) }}" target="_blank"><span class="badge rounded-pill text-bg-secondary p-2">{{ $tag->name }}</span></a>
+            <a href="{{ route('search.tags', $tag->slug) }}" target="_blank"><span class="badge rounded-pill text-bg-secondary p-2 m-2">{{ $tag->name }}</span></a>
         @endforeach
     </div>
 @endsection
