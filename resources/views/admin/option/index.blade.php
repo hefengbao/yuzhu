332133a7948ecccaa5a16e613f1ddcd4fc8d3a5d
hefengbao <754582231@qq.com>
2023-05-12 11:32:08 +0800

    添加换行

--- resources/views/admin/option/index.blade.php
+++ resources/views/admin/option/index.blade.php
@@ -12,24 +12,20 @@
                         <div class="form-group">
                             <label for="title" class="control-label col-md-2">站点标题</label>
                             <div class="col-md-10">
-                                <input type="text" id="title" name="title" class="form-control"
-                                       value="{{ $options['title'] }}" aria-describedby="titleHelp">
-                                <div id="titleHelp" class="form-text text-muted">起个有趣的名字吧</div>
+
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="subtitle" class="control-label col-md-2">站点副标题</label>
                             <div class="col-md-10">
-                                <input type="text" id="subtitle" name="subtitle" class="form-control"
-                                       value="{{ $options['subtitle'] }}" aria-describedby="subtitleHelp">
+                                <input type="text" id="subtitle" name="subtitle" class="form-control" value="{{ $options['subtitle'] }}" aria-describedby="subtitleHelp">
                                 <div id="subtitleHelp" class="form-text text-muted">用简洁的文字描述本站点</div>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="keyword" class="control-label col-md-2">站点关键词</label>
                             <div class="col-md-10">
-                                <textarea id="subtitle" name="keywords" rows="2" class="form-control"
-                                          aria-describedby="keywordsHelp">{{ $options['keywords'] }}</textarea>
+                                <textarea id="subtitle" name="keywords" rows="2" class="form-control" aria-describedby="keywordsHelp">{{ $options['keywords'] }}</textarea>
                                 <div id="keywordsHelp" class="form-text text-muted">
                                     定义个性化的关键词,请用英文逗号（,）分割
                                 </div>
@@ -52,7 +48,7 @@
                             <label for="icp" class="control-label col-md-2">搜索引擎验证 Meta</label>
                             <div class="col-md-10">
                                 <textarea name="site_verify_meta" id="site_verify_meta" rows="5" class="form-control" aria-describedby="siteVerifyMetaHelp">{{ $options['site_verify_meta'] ?? '' }}</textarea>
-                                <div id="siteVerifyMetaHelp" class="form-text text-muted">在常用的搜索引擎验证提交验证网站，可以借助平台提供的工具做一些运营分析等。百度：https://ziyuan.baidu.com， 必应：https://www.bing.com/webmasters，谷歌：https://search.google.com/search-console</div>
+                                <div id="siteVerifyMetaHelp" class="form-text text-muted">在常用的搜索引擎验证提交验证网站，可以借助平台提供的工具做一些运营分析等。\n百度：https://ziyuan.baidu.com \n必应：https://www.bing.com/webmasters \n谷歌：https://search.google.com/search-console</div>
                             </div>
                         </div>
                         <div class="form-group">
@@ -65,8 +61,7 @@
                         <div class="form-group">
                             <label for="users_can_register" class="control-label col-md-2">注册功能</label>
                             <div class="col-md-10">
-                                <select name="users_can_register" id="users_can_register" class="form-control"
-                                        aria-describedby="canRegisterHelp">
+                                <select name="users_can_register" id="users_can_register" class="form-control" aria-describedby="canRegisterHelp">
                                     <option value="0" @selected($options['users_can_register'])>关闭</option>
                                     <option value="1" @selected($options['users_can_register'])>开启</option>
                                 </select>
