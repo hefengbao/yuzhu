c64f27084bbdd2bb76920f7b087fb8934fb2d17f
hefengbao <754582231@qq.com>
2023-09-04 16:08:28 +0800

    update:使用 laravel/prompts 美化命令行表单

--- app/Console/Commands/InitAdminCommand.php
+++ app/Console/Commands/InitAdminCommand.php
@@ -5,6 +5,11 @@ namespace App\Console\Commands;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Console\Command;
+use function Laravel\Prompts\alert;
+use function Laravel\Prompts\error;
+use function Laravel\Prompts\password;
+use function Laravel\Prompts\text;
+use function Laravel\Prompts\warning;
 
 class InitAdminCommand extends Command
 {
@@ -32,23 +37,38 @@ class InitAdminCommand extends Command
         $admin = User::find(1);
 
         if ($admin) {
-            $this->info('已初始化管理员信息，请不要重复操作！');
+            alert('已初始化管理员信息，请不要重复操作！');
             return Command::SUCCESS;
         }
 
-        $this->info('开始初始化管理员账号，请按提示输入!');
+        info('开始初始化管理员账号，请按提示输入!');
 
-        $name = $this->ask('请输入用户名：');
+        $name = text(
+            label: '请输入用户名',
+            required: true
+        );
 
-        $email = $this->ask('请输入邮箱：');
+        $email = text(
+            label: '请输入邮箱',
+            required: true
+        );
 
-        $password = $this->secret('请输入密码：');
-        $confirm_password = $this->secret('请输入确认密码：');
+        $password = password(
+            label: '请输入密码',
+            required: true
+        );
+
+        $confirm_password = password(
+            label: '请输入确认密码',
+            required: true
+        );
 
         if ($password != $confirm_password) {
-            $this->error("确认密码不一致!");
+            error("确认密码不一致!");
             return Command::INVALID;
         }
+
+        info('请稍后...');
 
         $admin = new User();
         $admin->name = $name;
@@ -59,9 +79,9 @@ class InitAdminCommand extends Command
 
         event(new Registered($admin));
 
-        $this->info('一封验证邮件已发送到' . $email . ',请登录邮箱确认！');
+        info('一封验证邮件已发送到' . $email . ',请登录邮箱确认！');
 
-        $this->info('始化管理员信息结束！');
+        info('始化管理员信息结束！');
 
         return Command::SUCCESS;
     }
