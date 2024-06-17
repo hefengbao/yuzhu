5e024aa1e3af03c80a7522b7c3d7475a7379b63e
hefengbao <754582231@qq.com>
2024-06-17 16:06:33 +0800

    init api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,14 +1,18 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -18,5 +22,12 @@ return Application::configure(basePath: dirname(__DIR__))
         // 404 跳转到首页
         $exceptions->render(function (NotFoundHttpException $exception) {
             return redirect('/');
+        });
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => $e->getMessage(),
+                ], Response::HTTP_UNAUTHORIZED);
+            }
         });
     })->create();
