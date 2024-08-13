3bd908c2f9b2c5af8ca779b8a83a8c7a2970357a
hefengbao <754582231@qq.com>
2024-08-13 13:58:46 +0800

    添加字段

--- app/Http/Resources/V1/CommentResourece.php
+++ app/Http/Resources/V1/CommentResourece.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\V1;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Str;
 
 class CommentResourece extends JsonResource
 {
@@ -14,6 +15,19 @@ class CommentResourece extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'post_id' => $this->post_id,
+            'user_id' => $this->user_id,
+            'parent_id' => $this->poarent_id,
+            'body' => $this->body,
+            'body_html' => Str::markdown($this->body),
+            'guest_name' => $this->guest_name,
+            'guest_email' => $this->guest_email,
+            'status' => $this->status,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'author' => new UserResource($this->author)
+        ];
     }
 }
