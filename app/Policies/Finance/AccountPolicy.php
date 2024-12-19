c432244795e4fb3c6626d046519bdc2e79b34121
hefengbao <754582231@qq.com>
2024-12-19 10:44:59 +0800

    fix: 修复权限控制问题

--- app/Policies/Finance/AccountPolicy.php
+++ app/Policies/Finance/AccountPolicy.php
@@ -12,7 +12,7 @@ class AccountPolicy
      */
     public function viewAny(User $user): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Policies/Finance/TransactionPolicy.php
+++ app/Policies/Finance/TransactionPolicy.php
@@ -12,7 +12,7 @@ class TransactionPolicy
      */
     public function viewAny(User $user): bool
     {
-        return false;
+        return true;
     }
 
     /**
